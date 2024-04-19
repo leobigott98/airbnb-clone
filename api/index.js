3fa7a406429c4459978a7b372d2af6403c4e38ce
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-04-19 18:55:54 -0400

    implemented login

--- api/index.js
+++ api/index.js
@@ -5,10 +5,12 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = process.env.JWT_SECRET;
 
 app.use(
   cors({
@@ -39,5 +41,29 @@ app.post("/register", async (req, res) => {
     res.status(422).json(e);
   }
 });
+
+app.post('/login', async (req,res)=>{
+  const {email, password} = req.body;
+
+  try {
+    const userDoc = await User.findOne({email});
+    if (userDoc) {
+      const passOk = bcrypt.compareSync(password, userDoc.password);
+      if(passOk){
+        jwt.sign({email: userDoc.email, id: userDoc._id}, jwtSecret, {}, (err, token)=>{
+          if(err) throw err;
+          res.cookie('token', token).json('pass ok');
+        });
+        
+      }else{
+        res.status(422).json('pass not ok')
+      }
+    }else{
+      res.json('not found');
+    }
+  } catch (error) {
+    
+  }
+})
 
 app.listen(4000);
