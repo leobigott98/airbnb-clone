1227b227a696acaa79ea3dac80125a6b7e6af55c
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-04-20 00:07:05 -0400

    added account page and logout functionality

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -6,16 +6,18 @@ export const UserContext = createContext({});
 // eslint-disable-next-line react/prop-types
 export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [ready, setReady] = useState(false);
   useEffect(()=>{
     if(!user){
       axios.get('/profile').then((data)=>{
-        setUser(data.data)
+        setUser(data.data);
+        setReady(true);
       })
       
     }
   },[user]);
     return (
-    <UserContext.Provider value={{user, setUser}}>
+    <UserContext.Provider value={{user, setUser, ready}}>
         {children}
     </UserContext.Provider>);
 }
