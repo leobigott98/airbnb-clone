a68c90bd22696cf928d4fe475739de0fbfc90d88
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-05-14 01:11:30 -0400

    added booking functionality

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ const cookieParser = require("cookie-parser");
 const imageDownloader = require("image-downloader");
 const multer = require("multer");
 const fs = require("fs");
+const Booking = require('./models/Booking');
 
 const app = express();
 
@@ -118,59 +119,105 @@ app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
 
 app.post("/places", (req, res) => {
   const { token } = req.cookies;
-  const {title, address, addedPhotos, description, 
-    perks, extraInfo, checkIn, checkOut, maxGuests, price} = req.body;
+  const {
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
+  } = req.body;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     if (err) throw err;
     const placeDoc = await Place.create({
       owner: userData.id,
-      title, address, 
-      photos: addedPhotos, 
-      description, 
-      perks, extraInfo, checkIn, checkOut, maxGuests, price
+      title,
+      address,
+      photos: addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+      price,
     });
     res.json(placeDoc);
-  }); 
+  });
 });
 
-app.get("/user-places", (req, res)=>{
+app.get("/user-places", (req, res) => {
   const { token } = req.cookies;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     if (err) throw err;
-    const {id} = userData;
-    res.json( await Place.find({owner:id}));
-  }); 
-
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
 });
 
-app.get("/places/:id", async (req, res)=>{
-  const {id} = req.params;
+app.get("/places/:id", async (req, res) => {
+  const { id } = req.params;
   res.json(await Place.findById(id));
 });
 
-app.put('/places', async(req,res)=>{
+app.put("/places", async (req, res) => {
   const { token } = req.cookies;
-  const {id, title, address, addedPhotos, description, 
-    perks, extraInfo, checkIn, checkOut, maxGuests, price} = req.body;
+  const {
+    id,
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
+  } = req.body;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-    if(err) throw err;
+    if (err) throw err;
     const placeDoc = await Place.findById(id);
-    if (userData.id === placeDoc.owner.toString()){
+    if (userData.id === placeDoc.owner.toString()) {
       placeDoc.set({
-        title, address, 
-        photos: addedPhotos, 
-        description, 
-        perks, extraInfo, checkIn, checkOut, maxGuests, price
+        title,
+        address,
+        photos: addedPhotos,
+        description,
+        perks,
+        extraInfo,
+        checkIn,
+        checkOut,
+        maxGuests,
+        price,
       });
       await placeDoc.save();
-      res.json('ok')
+      res.json("ok");
     }
-  })
+  });
 });
 
-app.get('/places', async (req, res)=>{
+app.get("/places", async (req, res) => {
   res.json(await Place.find());
-})
+});
+
+app.post("/bookings", (req, res) => {
+  const { 
+    place, checkIn, checkOut, numberOfGuests, name, phone 
+  } = req.body;
+  Booking.create({
+    place, checkIn, checkOut, numberOfGuests, name, phone
+  }).then((doc)=>{
+    res.json(doc);
+  }).catch((err)=>{
+    throw err;
+  })
+
+});
 
 app.listen(4000, () => {
   console.log("listening on port 4000");
--- client/src/components/BookingWidget.jsx
+++ client/src/components/BookingWidget.jsx
@@ -1,30 +1,96 @@
-/* eslint-disable react/prop-types */
-export default function BookingWidget({place}) {
-    return(
-        <div>
-          <div className="bg-white shqdow p-4 rounded-2xl">
-            <div className="text-2xl text-center">
-              Price: ${place.price} / per night
-            </div>
-            <div className="border rounded-2xl mt-4">
-              <div className="flex">
-                <div className="p-3 px-4">
-                  <label>Check-in:</label>
-                  <input type="date" />
-                </div>
-                <div className="p-3 px-4 border-l">
-                  <label>Check-out:</label>
-                  <input type="date" />
-                </div>
-              </div>
-              <div className="p-3 px-4 border-t">
-                <label>Number of guests:</label>
-                <input type="number" value={1} />
-              </div>
-            </div>
+import { useState } from "react";
+import { differenceInCalendarDays } from "date-fns";
+import axios from "axios";
+import {Navigate} from "react-router-dom";
 
-            <button className="primary mt-4">Book this place</button>
-          </div>
-        </div>
+/* eslint-disable react/prop-types */
+export default function BookingWidget({ place }) {
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [redirect, setRedirect] = useState('');
+
+  let numberOfNights = 0;
+
+  if (checkIn && checkOut) {
+    numberOfNights = differenceInCalendarDays(
+      new Date(checkOut),
+      new Date(checkIn)
     );
+  }
+
+  async function bookThisPlace(){
+    const response = await axios.post('/bookings', {
+        checkIn, checkOut, numberOfGuests, name, phone,
+        place: place._id, 
+        price: numberOfNights * place.price,
+    });
+    const bookingId = response.data._id;
+    setRedirect(`/account/bookings/${bookingId}`);
+  }
+
+  if(redirect){
+    return <Navigate to={redirect}/>
+  }
+
+  return (
+    <div>
+      <div className="bg-white shqdow p-4 rounded-2xl">
+        <div className="text-2xl text-center">
+          Price: ${place.price} / per night
+        </div>
+        <div className="border rounded-2xl mt-4">
+          <div className="flex">
+            <div className="p-3 px-4">
+              <label>Check-in:</label>
+              <input
+                type="date"
+                value={checkIn}
+                onChange={(e) => setCheckIn(e.target.value)}
+              />
+            </div>
+            <div className="p-3 px-4 border-l">
+              <label>Check-out:</label>
+              <input
+                type="date"
+                value={checkOut}
+                onChange={(e) => setCheckOut(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="p-3 px-4 border-t">
+            <label>Number of guests:</label>
+            <input
+              type="number"
+              value={numberOfGuests}
+              onChange={(e) => setNumberOfGuests(e.target.value)}
+            />
+          </div>
+          {numberOfNights > 0 && (
+            <div className="p-3 px-4 border-t">
+            <label>Your full name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label>Phone number:</label>
+            <input
+              type="tel"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          )}
+        </div>
+
+        <button onClick={bookThisPlace} className="primary mt-4">
+          Book this place
+          {numberOfNights > 0 && <span> ${numberOfNights * place.price}</span>}
+        </button>
+      </div>
+    </div>
+  );
 }
